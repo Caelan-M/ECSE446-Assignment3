40d6e896d3a29bdf571f430fd819e9ee72c9b372
Caelan Marks <caelan.marks@mail.mcgill.ca>
2018-10-17 17:36:09 -0400

    bonus combined with non-bonus for realtime

--- src/shaders/ssao.fs
+++ src/shaders/ssao.fs
@@ -92,37 +92,99 @@ float depthRange(vec3 origin, float depthAtSample)
 // ------------------------------------------------------------------
 void main()
 {
-    setRNGSeed();
+    bool bonus = false;
+    if(bonus){
+        setRNGSeed();
 
-    //1) Get the position and normal of the shading point (screen space) from the GBuffer.
-    vec4 shadePosition = texture(texturePosition, texCoords);
-    vec4 shadeNormal = texture(textureNormal, texCoords);
+        //1) Get the position and normal of the shading point (screen space) from the GBuffer.
+        vec4 shadePosition = texture(texturePosition, texCoords);
+        vec4 shadeNormal = texture(textureNormal, texCoords);
 
-    //1) Build the shading normal's frame (TBN).
-    // TBN - M3[V3(Tangent)|V3(Bitangent)|V3(Normal)]
-    vec3 shadeTangent = getTangent(shadeNormal.xyz);
-    vec3 shadeBitangent = normalize(cross(shadeNormal.xyz, shadeTangent));
-    mat3 TBN = mat3(shadeTangent, shadeBitangent, shadeNormal);
+        //1) Build the shading normal's frame (TBN).
+        // TBN - M3[V3(Tangent)|V3(Bitangent)|V3(Normal)]
+        vec3 shadeTangent = getTangent(shadeNormal.xyz);
+        vec3 shadeBitangent = normalize(cross(shadeNormal.xyz, shadeTangent));
+        mat3 TBN = mat3(shadeTangent, shadeBitangent, shadeNormal);
 
-    color = vec3(0.f);
+        color = vec3(0.f);
 
-    for(int i = 0; i < N_SAMPLES; i++){
+        for(int i = 0; i < N_SAMPLES; i++){
 
-        //1) Get a sample direction (view space).
-        vec3 sampleDir = squareToUniformHemisphere();
-        float cosFact = sampleDir.z;
-        float pdf = squareToUniformHemispherePdf();
+            //1) Get a sample direction (view space).
+            vec3 sampleDir = squareToUniformHemisphere();
+            float cosFact = sampleDir.z;
+            float pdf = squareToUniformHemispherePdf();
 
-        //2) Align the sample hemisphere to the shading normal.
-        vec3 alignedDir = TBN * sampleDir;
+            //2) Align the sample hemisphere to the shading normal.
+            vec3 alignedDir = TBN * sampleDir;
 
-        //sampling many points along the sampled direction
-        int pps = 36; //(how many points per sample direction)
-        vec3 tempColor = vec3(1.f);
-        for(int j = 0; j < pps; j++){
+            //sampling many points along the sampled direction
+            int pps = 36; //(how many points per sample direction)
+            vec3 tempColor = vec3(1.f);
+            for(int j = 0; j < pps; j++){
+                //3) Place the sample at the shading point (use the RADIUS constant).
+                // Shading point = starting position + (aligned direction * distance traveled)
+                vec3 shadingPoint = shadePosition.xyz + (alignedDir * j * RADIUS / 3);
+
+                //4) Get the depth value at the sample's position using the depth buffer.
+                // - Project the sample to screen space ie. pixels (NDC).
+                // take shading point and project onto screen space
+                vec4 screenPoint = projection * vec4(shadingPoint, 1.f);
+
+                // - Transform the sample in NDC coordinates [-1,1] to texture coordinates [0,1].
+                mat4 biasMatrix = mat4(
+                            0.5, 0.0, 0.0, 0.0,
+                            0.0, 0.5, 0.0, 0.0,
+                            0.0, 0.0, 0.5, 0.0,
+                            0.5, 0.5, 0.5, 1.0
+                    );
+                vec4 depthPoint = biasMatrix * screenPoint;
+                depthPoint.xyz = depthPoint.xyz / depthPoint.w;
+
+                //5) Check for occlusion using the sample's depth value and the depth value at the sample's position.
+                //(use some epsilon via the BIAS constant)
+                float depthAtPos = texture(texturePosition, depthPoint.xy).z;
+                float visible = (depthAtPos > (shadingPoint.z - BIAS) ? 1.f : 0.f) * smoothstep(0.0, 1.0, j / pps);
+
+                //Check if point on direction is occluded, if so, break
+                if(depthAtPos > (shadingPoint.z - BIAS)){
+                    tempColor = vec3(visible);
+                    break;
+                }
+
+            }
+            color += tempColor * INV_PI / pdf * max(0.f, cosFact);
+        }
+        color = color / N_SAMPLES;
+    }
+    else{
+       setRNGSeed();
+
+        //1) Get the position and normal of the shading point (screen space) from the GBuffer.
+        vec4 shadePosition = texture(texturePosition, texCoords);
+        vec4 shadeNormal = texture(textureNormal, texCoords);
+
+        //1) Build the shading normal's frame (TBN).
+        // TBN - M3[V3(Tangent)|V3(Bitangent)|V3(Normal)]
+        vec3 shadeTangent = getTangent(shadeNormal.xyz);
+        vec3 shadeBitangent = normalize(cross(shadeNormal.xyz, shadeTangent));
+        mat3 TBN = mat3(shadeTangent, shadeBitangent, shadeNormal);
+
+        color = vec3(0.f);
+
+        for(int i = 0; i < N_SAMPLES; i++){
+
+            //1) Get a sample direction (view space).
+            vec3 sampleDir = squareToUniformHemisphere();
+            float cosFact = sampleDir.z;
+            float pdf = squareToUniformHemispherePdf();
+
+            //2) Align the sample hemisphere to the shading normal.
+            vec3 alignedDir = TBN * sampleDir;
+
             //3) Place the sample at the shading point (use the RADIUS constant).
             // Shading point = starting position + (aligned direction * distance traveled)
-            vec3 shadingPoint = shadePosition.xyz + (alignedDir * j * RADIUS / 3);
+            vec3 shadingPoint = shadePosition.xyz + (alignedDir * RADIUS);
 
             //4) Get the depth value at the sample's position using the depth buffer.
             // - Project the sample to screen space ie. pixels (NDC).
@@ -136,22 +198,16 @@ void main()
                         0.0, 0.0, 0.5, 0.0,
                         0.5, 0.5, 0.5, 1.0
                 );
-            vec4 depthPoint = biasMatrix * screenPoint;
-            depthPoint.xyz = depthPoint.xyz / depthPoint.w;
+            screenPoint = biasMatrix * screenPoint;
+            screenPoint.xyz = screenPoint.xyz / screenPoint.w;
 
             //5) Check for occlusion using the sample's depth value and the depth value at the sample's position.
             //(use some epsilon via the BIAS constant)
-            float depthAtPos = texture(texturePosition, depthPoint.xy).z;
-            float visible = (depthAtPos > (shadingPoint.z - BIAS) ? 1.f : 0.f) * smoothstep(0.0, 1.0, j / pps);
+            float depthAtPos = texture(texturePosition, screenPoint.xy).z;
+            float visible = depthAtPos < (shadingPoint.z - BIAS) ? 1.f : 0.f;
 
-            //Check if point on direction is occluded, if so, break
-            if(depthAtPos > (shadingPoint.z - BIAS)){
-                tempColor = vec3(visible);
-                break;
-            }
-
+            color += vec3(visible) * INV_PI / pdf * max(0.f, cosFact);
         }
-        color += tempColor * INV_PI / pdf * max(0.f, cosFact);
+        color = color / N_SAMPLES;
     }
-    color = color / N_SAMPLES;
 }
